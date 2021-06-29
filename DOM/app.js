5738dfa8088a654d74ce4be63f01fae9036f5973
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-06-29 22:34:23 +0200

    dom

--- DOM/app.js
+++ DOM/app.js
@@ -1,8 +1,18 @@
-const back = document.querySelector('#result');
-console.log(back);
-back.style.backgroundColor = 'blue';
+const result = document.querySelector('.result');
+const h2 = document.querySelector('.red');
+const bodyDiv = document.createElement('div');
+const bodyText = document.createTextNode('hello body');
+bodyDiv.appendChild(bodyText);
+document.body.insertBefore(bodyDiv, result);
 
-const items = document.querySelectorAll('.special');
-items.forEach((item) => (item.style.color = 'red'));
-const lastItem = document.querySelector('.last');
-lastItem.style.color = 'yellow';
+const heading = document.createElement('h2');
+const text = document.createTextNode('je suis un h2');
+heading.appendChild(text);
+heading.classList.add('blue');
+result.insertBefore(heading, h2);
+
+const smallHeading = document.createElement('h6');
+const smallText = document.createTextNode('je suis tout petit');
+smallHeading.classList.add('red');
+smallHeading.appendChild(smallText);
+document.body.replaceChild(smallHeading, bodyDiv);
