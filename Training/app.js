067320ac66efeae29fbd78df0375b975c018941f
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-07-06 23:33:49 +0200

    object udemy videos

--- Training/app.js
+++ Training/app.js
@@ -1,15 +1,18 @@
-let arr = ['apple', 'pear', 'grapes'];
-
-const fruitSalad = (arr) => {
-  let finalArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    let wordLenght = arr[i].length / 2;
-    let partOne = arr[i].substr(0, wordLenght);
-    let partTwo = arr[i].substr(wordLenght, wordLenght * 2);
-    finalArr.push(partOne);
-    finalArr.push(partTwo);
-  }
-  return console.log(finalArr.sort().join(''));
+const counter = {
+  count: 0,
+  increment() {
+    console.log(this);
+    this.count++;
+    console.log(this.count);
+  },
 };
+const btn = document.querySelector('.increment');
 
-fruitSalad(['banana']);
+// btn.addEventListener('click', counter.increment);
+
+// some edge cases
+// btn.addEventListener('click', counter.increment.bind(counter));
+
+const increment = counter.increment.bind(counter);
+btn.addEventListener('click', increment);
+btn.removeEventListener('click', increment);
