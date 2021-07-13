90dc879dee8618b990f0034aae6b4fdede54545a
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-07-13 18:59:44 +0200

    ES6 freeze

--- Training/app.js
+++ Training/app.js
@@ -1,14 +1,5 @@
-const p = document.querySelectorAll('p');
-const result = document.getElementById('result');
-const second = document.getElementById('second');
+const mathConstant = {
+  PI: 3.14,
+};
 
-let newText = Array.from(p);
-newText = newText.map((item) => `<span>${item.textContent}</span>`).join(' ');
-
-result.innerHTML = newText;
-
-const text = Array.from(document.querySelectorAll('p'), (item) => {
-  return `<span>${item.textContent}</span>`;
-}).join(' ');
-
-second.innerHTML = text;
+Object.freeze(mathConstant);
