798d3368088e8a20508d77b0c43217e9797a08d2
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-07-19 18:22:14 +0200

    AJAX course

--- Training/app.js
+++ Training/app.js
@@ -1,42 +1,23 @@
-// async function someFunction(){
-//   await
-// }
-
-// const someFunction = async()=>{
-//   await
-// }
-
-const heanding1 = document.querySelector('.one');
-const heanding2 = document.querySelector('.two');
-const heanding3 = document.querySelector('.three');
-
 const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', async () => {
-  const result = await displayColor();
-  console.log(result);
+btn.addEventListener('click', () => {
+  getData();
 });
 
-async function displayColor() {
-  try {
-    await addColor(1000, heanding1, 'red');
-    await addColor(1000, heanding2, 'green');
-    await addColor(1000, heanding3, 'blue');
-  } catch (error) {
-    console.log(error);
-  }
-  return 'hello';
-}
+function getData() {
+  const xhr = new XMLHttpRequest();
 
-function addColor(time, element, color) {
-  return new Promise((resolve, reject) => {
-    if (element) {
-      setTimeout(() => {
-        element.style.color = color;
-        resolve();
-      }, time);
+  xhr.open('GET', '../api/simple.txt');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const text = document.createElement('p');
+      text.textContent = xhr.responseText;
+      document.body.appendChild(text);
     } else {
-      reject(new Error(`There is no such element ${element}`));
+      console.log({
+        satuts: xhr.status,
+        text: xhr.statusText,
+      });
     }
-  });
+  };
+  xhr.send();
 }
