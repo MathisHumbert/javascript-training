eafa9f9489f2aa2929aa5005a6fee2501c224962
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-08 14:18:17 +0200

    new FCC project done

--- Training/app.js
+++ Training/app.js
@@ -1,5 +1,12 @@
 function frankenSplice(arr1, arr2, n) {
-  return arr2;
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
 }
-
-frankenSplice([1, 2, 3], [4, 5, 6], 1);
+console.log(
+  frankenSplice(['claw', 'tentacle'], ['head', 'shoulders', 'knees', 'toes'], 2)
+);
