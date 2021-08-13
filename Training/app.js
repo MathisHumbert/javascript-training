a11c79271b1f19b86b0d0664b2f1e1ffbc67f73b
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-13 10:37:55 +0200

    Eloquent JS example

--- Training/app.js
+++ Training/app.js
@@ -1,6 +1,7 @@
-let result = Number(prompt('Enter passcode'));
-if (Number.isNaN(result)) {
-  console.log('you must enter a number');
-} else {
-  console.log(`Your number is the square root of ${result * result}`);
-}
+let result;
+
+do {
+  result = Number(prompt('Enter a number'));
+} while (Number.isNaN(result));
+
+console.log(`Your number is the square root of ${result * result}`);
