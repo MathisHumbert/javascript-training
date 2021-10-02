a184ed0790a92218978c5f6d8f0179828bad2f3e
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-02 19:02:30 +0200

    codewars challenges solution

--- Training/app.js
+++ Training/app.js
@@ -1,15 +1,45 @@
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function zero() {
+  return 0;
+}
+function one() {
+  return 1;
+}
+function two() {
+  return 2;
+}
+function three() {
+  return 3;
+}
+function four() {
+  return 4;
+}
+function five() {
+  return 5;
+}
+function six() {
+  return 6;
+}
+function seven(num) {
+  if (num) {
+    console.log(num);
+  } else {
+    return 7;
+  }
+}
+function eight(num) {
+  return 8;
+}
+function nine() {
+  return 9;
+}
 
 function plus() {}
 function minus() {}
 function times() {}
-function dividedBy() {}
+function dividedBy(num) {
+  return ['/', num];
+}
+
+console.log(eight(dividedBy(seven())));
+
+// faire la meme fonction de la 7 sur toutes et return selon l'array recu
