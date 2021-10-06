590d55482562c3d25ecfca10b3cfd8b58f2d8c49
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-06 15:13:57 +0200

    promises course

--- Training/app.js
+++ Training/app.js
@@ -1,19 +1,26 @@
 // callbacks, promises, async/await
+// PROMISES - Pending, Resolved, Rejected
+// then catch - pass another callback
 
 const heading1 = document.querySelector('.one');
 const heading2 = document.querySelector('.two');
 const heading3 = document.querySelector('.three');
-
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
-  setTimeout(() => {
-    heading1.style.color = 'red';
-    setTimeout(() => {
-      heading2.style.color = 'green';
-      setTimeout(() => {
-        heading3.style.color = 'blue';
-      }, 1000);
-    }, 1000);
-  }, 1000);
+  addColor(1000, heading1, 'red')
+    .then(() => addColor(1000, heading2, 'green'))
+    .then(() => addColor(1000, heading3, 'blue'))
+    .catch((err) => console.log(err));
 });
+
+function addColor(time, element, color) {
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else reject(new Error(`There is no such element ${element}`));
+  });
+}
