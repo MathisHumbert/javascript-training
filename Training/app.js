4628027a061a720bc253d159e5114b7acec0b0be
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-06 15:32:55 +0200

    async and await course

--- Training/app.js
+++ Training/app.js
@@ -2,17 +2,34 @@
 // PROMISES - Pending, Resolved, Rejected
 // then catch - pass another callback
 
+// async function someFunction (){
+//  await
+// }
+
+// const otherFunction = async() => {
+//  await
+// }
+
 const heading1 = document.querySelector('.one');
 const heading2 = document.querySelector('.two');
 const heading3 = document.querySelector('.three');
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', () => {
-  addColor(1000, heading1, 'red')
-    .then(() => addColor(1000, heading2, 'green'))
-    .then(() => addColor(1000, heading3, 'blue'))
-    .catch((err) => console.log(err));
+btn.addEventListener('click', async () => {
+  const result = await displayColor();
+  console.log(result);
 });
+
+async function displayColor() {
+  try {
+    await addColor(1000, heading1, 'red');
+    await addColor(1000, heading2, 'green');
+    await addColor(1000, heading3, 'blue');
+  } catch (error) {
+    console.log(error);
+  }
+  return 'hello';
+}
 
 function addColor(time, element, color) {
   return new Promise((resolve, reject) => {
