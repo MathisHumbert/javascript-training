df38ee7248f54977fde8bc65ba1bf69b141484ec
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-07 11:37:48 +0200

    json course

--- Training/app.js
+++ Training/app.js
@@ -1,14 +1,21 @@
 const btn = document.querySelector('.btn');
-btn.addEventListener('click', getData);
+const url = './api/people.json';
+btn.addEventListener('click', () => {
+  getData(url);
+});
 
-function getData() {
+function getData(url) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', './api/sample.txt');
+  xhr.open('GET', url);
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      const text = document.createElement('p');
-      text.textContent = xhr.responseText;
-      document.body.appendChild(text);
+      const data = JSON.parse(xhr.responseText);
+      const displaydDtat = data.map((item) => {
+        return `<p>${item.name}</p>`;
+      });
+      const element = document.createElement('div');
+      element.innerHTML = displaydDtat.join('');
+      document.body.appendChild(element);
     } else {
       console.log({ status: xhr.status, text: xhr.statusText });
     }
