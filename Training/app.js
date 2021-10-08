b92d12538b3838e444711baad2dd0f4f45bcfe1a
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-08 09:02:16 +0200

    codewar challenge done

--- Training/app.js
+++ Training/app.js
@@ -1,1 +1,8 @@
+function squareDigits(num) {
+  let numArr = num.toString().split('');
+  numArr = numArr.map((num) => Math.pow(parseInt(num), 2)).join('');
+  return parseInt(numArr);
+}
 
+console.log(squareDigits(9119));
+console.log(squareDigits(3212));
