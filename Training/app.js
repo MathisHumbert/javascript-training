9899e4311b01919fe88f54b20567c264434d052f
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-14 11:08:28 +0200

    training class

--- Training/app.js
+++ Training/app.js
@@ -1,31 +1,58 @@
-class Page {
-  constructor(text) {
-    this.text = text;
+class Publication {
+  constructor(title, author, pubDate) {
+    this.title = title;
+    this.author = author;
+    this.pubDate = pubDate;
   }
 
   print() {
-    console.log(this.text);
+    console.log(`
+    Title: ${this.title} 
+    By: ${this.author} 
+    ${this.pubDate}`);
   }
 }
 
-class Notebook {
-  constructor() {
-    this.pages = [];
-  }
+class Book extends Publication {
+  constructor(bookDtails) {
+    super(bookDtails.title, bookDtails.author, bookDtails.publishedOn);
 
-  addPage(text) {
-    let page = new Page(text);
-    this.pages.push(page);
+    this.publisher = bookDtails.publisher;
+    this.ISBN = bookDtails.ISBn;
   }
 
   print() {
-    for (let page of this.pages) {
-      page.print();
-    }
+    super.print();
+    console.log(`Publisher: ${this.publisher} 
+   ISBN: ${this.ISBN}`);
   }
 }
 
-let mathBook = new Notebook();
-mathBook.addPage('edddddddd');
-mathBook.addPage('eeded');
-mathBook.print();
+class BlogPost extends Publication {
+  constructor(title, author, pubDate, URL) {
+    super(title, author, pubDate);
+    this.URL = URL;
+  }
+  print() {
+    super.print();
+    console.log(`ISBN: ${this.URL}`);
+  }
+}
+
+let YDKJS = new Book({
+  title: "You Don't Know JS",
+  author: 'Kyle Simpson',
+  publishedOn: 'June 2014',
+  publisher: "O'Reilly",
+  ISBN: '123456-789',
+});
+
+YDKJS.print();
+
+let forAgainstLet = new BlogPost(
+  'For and against let',
+  'Kyle Sympson',
+  'October 27, 2014',
+  'https://jdacdjncdi'
+);
+forAgainstLet.print();
