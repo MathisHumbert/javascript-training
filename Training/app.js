b5f2bba0a76273694a8380ec28e3e6137bd7c3e8
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-15 08:45:44 +0200

    practicing iterator

--- Training/app.js
+++ Training/app.js
@@ -1,15 +1,25 @@
-function createPerson(firstName, lastName) {
-  let publicAPI = {
-    firstName,
-    lastName,
-    fullName: function () {
-      console.log(`My name is ${this.firstName} ${this.lastName}`);
-    },
-  };
-  return publicAPI;
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+let arr = [10, 20, 30];
+
+let buttonNames = new Map();
+buttonNames.set(btn1, 'Button 1');
+buttonNames.set(btn2, 'Button 2');
+
+for (let [btn, btnName] of buttonNames) {
+  console.log(btn, btnName);
+  btn.addEventListener('click', function () {
+    console.log(btnName);
+  });
 }
 
-let person = createPerson('Mathis', 'Humbert');
-person.fullName();
-let person2 = createPerson('Lisa', 'Mora');
-person2.fullName();
+for (let btnName of buttonNames.values()) {
+  console.log(btnName);
+}
+for (let btn of buttonNames.keys()) {
+  console.log(btn);
+}
+
+for (let [idx, val] of arr.entries()) {
+  console.log(`${[idx]}: ${val}`);
+}
