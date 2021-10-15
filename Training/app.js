322280b336cf58c255d5c52030a340ae34d7e170
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-15 10:04:33 +0200

    this practice

--- Training/app.js
+++ Training/app.js
@@ -1,68 +1,19 @@
-function greet(msg) {
-  return function who(name) {
-    console.log(`${msg}, ${name}`);
+function classroom(teacher) {
+  return function study() {
+    console.log(`${teacher}, says to study ${this.topic}`);
   };
 }
 
-let hello = greet('hello');
-hello('Mathis');
-hello('Lisa');
+let assignement = classroom('Kyle');
 
-function counter(step = 1) {
-  let count = 0;
-  return function increaseCount() {
-    count = count + step;
-    return count;
-  };
-}
-
-let incBy1 = counter(1);
-incBy1();
-incBy1();
-
-let incBy7 = counter(7);
-incBy7();
-incBy7();
-
-function getSomeData(url) {
-  ajax(url, function onResponse(resp) {
-    console.log(`Response (from ${url}): ${resp}`);
-  });
-}
-
-function addA(x) {
-  return function (y) {
-    return x + y;
-  };
-}
-
-let add5 = addA(5);
-let add10 = addA(10);
-console.log(add5(5));
-console.log(add10(3));
-
-let creerCompteur = function () {
-  let compteurPrive = 0;
-  function changeValeur(val) {
-    compteurPrive += val;
-  }
-  return {
-    increment: function () {
-      changeValeur(1);
-    },
-    decrement: function () {
-      changeValeur(-1);
-    },
-    valeur: function () {
-      return compteurPrive;
-    },
-  };
+let homework = {
+  topic: 'JS',
+  assignement: assignement,
 };
 
-let compteur1 = creerCompteur();
-console.log(compteur1.valeur());
-compteur1.increment();
-compteur1.increment();
-console.log(compteur1.valeur());
-compteur1.decrement();
-console.log(compteur1.valeur());
+let otherHomwork = {
+  topic: 'Math',
+};
+
+homework.assignement();
+assignement.call(otherHomwork);
