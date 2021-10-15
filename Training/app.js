a0425519770fb886877487408073ddcb6976c9ec
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-15 09:49:29 +0200

    closure practice

--- Training/app.js
+++ Training/app.js
@@ -1,25 +1,68 @@
-const btn1 = document.querySelector('.btn1');
-const btn2 = document.querySelector('.btn2');
-let arr = [10, 20, 30];
+function greet(msg) {
+  return function who(name) {
+    console.log(`${msg}, ${name}`);
+  };
+}
 
-let buttonNames = new Map();
-buttonNames.set(btn1, 'Button 1');
-buttonNames.set(btn2, 'Button 2');
+let hello = greet('hello');
+hello('Mathis');
+hello('Lisa');
 
-for (let [btn, btnName] of buttonNames) {
-  console.log(btn, btnName);
-  btn.addEventListener('click', function () {
-    console.log(btnName);
+function counter(step = 1) {
+  let count = 0;
+  return function increaseCount() {
+    count = count + step;
+    return count;
+  };
+}
+
+let incBy1 = counter(1);
+incBy1();
+incBy1();
+
+let incBy7 = counter(7);
+incBy7();
+incBy7();
+
+function getSomeData(url) {
+  ajax(url, function onResponse(resp) {
+    console.log(`Response (from ${url}): ${resp}`);
   });
 }
 
-for (let btnName of buttonNames.values()) {
-  console.log(btnName);
-}
-for (let btn of buttonNames.keys()) {
-  console.log(btn);
+function addA(x) {
+  return function (y) {
+    return x + y;
+  };
 }
 
-for (let [idx, val] of arr.entries()) {
-  console.log(`${[idx]}: ${val}`);
-}
+let add5 = addA(5);
+let add10 = addA(10);
+console.log(add5(5));
+console.log(add10(3));
+
+let creerCompteur = function () {
+  let compteurPrive = 0;
+  function changeValeur(val) {
+    compteurPrive += val;
+  }
+  return {
+    increment: function () {
+      changeValeur(1);
+    },
+    decrement: function () {
+      changeValeur(-1);
+    },
+    valeur: function () {
+      return compteurPrive;
+    },
+  };
+};
+
+let compteur1 = creerCompteur();
+console.log(compteur1.valeur());
+compteur1.increment();
+compteur1.increment();
+console.log(compteur1.valeur());
+compteur1.decrement();
+console.log(compteur1.valeur());
