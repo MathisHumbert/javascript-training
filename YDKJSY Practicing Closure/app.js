4d5d8a7094441750097f84ad062676b84edb9929
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-16 10:51:38 +0200

    practicing closure book

--- YDKJSY Practicing Closure/app.js
+++ YDKJSY Practicing Closure/app.js
@@ -1,33 +1,21 @@
 // This is my solution for YDKJSY practice
 
-const dayStart = '7:30';
-const dayEnd = '17:45';
+function range(start, end) {
+  if (end == undefined) {
+    return function getEnd(end) {
+      return getRange(start, end);
+    };
+  } else return getRange(start, end);
 
-function scheduleMeeting(startTime, durationMinutes) {
-  startTime = startTime.split(':').map((time) => parseInt(time));
-  if (parseInt(dayStart.split(':')[0]) > startTime[0]) return false;
-
-  if (
-    parseInt(dayStart.split(':')[0]) == startTime[0] &&
-    parseInt(dayStart.split(':')[1]) > startTime[1]
-  )
-    return false;
-
-  if (startTime[1] + durationMinutes > 60) {
-    startTime[0]++;
-    startTime[1] + durationMinutes - 60;
-  } else {
-    startTime[1] += durationMinutes;
+  function getRange(start, end) {
+    let arr = [];
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
+    }
+    return arr;
   }
-
-  console.log(parseInt(dayEnd.split(':')[1]));
-
-  if (parseInt(dayEnd.split(':')[0]) < startTime[0]) {
-    return false;
-  } else if (parseInt(dayEnd.split(':')[0]) == startTime[0]) {
-    if (parseInt(dayEnd.split(':')[1]) >= startTime[1]) return true;
-    else return false;
-  } else return true;
 }
 
-console.log(scheduleMeeting('18:00', 15));
+console.log(range(3, 10));
+let start3 = range(1);
+console.log(start3(10));
