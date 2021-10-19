da02a43a31f7712d36bd2d6289e0909452288e80
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-19 15:23:07 +0200

    front end master oop course

--- Training/app.js
+++ Training/app.js
@@ -1,3 +1,4 @@
+// Solution 2
 function userCreator(name, score) {
   const newUser = Object.create(userFunctions);
   newUser.name = name;
@@ -34,6 +35,16 @@ Object.setPrototypeOf(paidUserFunctions, userFunctions);
 
 const paidUser1 = paidUserCreator('Alyssa', 8, 25);
 
-paidUser1.increaseBalance();
-console.log(paidUser1);
-paidUser1.sayName();
+// Call
+const obj = {
+  num: 3,
+  increment: function () {
+    this.num++;
+  },
+};
+
+const otherObj = { num: 10 };
+obj.increment();
+console.log(obj);
+obj.increment.call(otherObj);
+console.log(otherObj);
