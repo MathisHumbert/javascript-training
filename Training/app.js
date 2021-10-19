8f2057f86e0cb67bbdfc56124a5bc3e87351f7ca
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-19 16:48:19 +0200

    front end master oop course

--- Training/app.js
+++ Training/app.js
@@ -50,36 +50,72 @@
 // console.log(otherObj);
 
 // Solution 3
-function userCreator(name, score) {
-  this.name = name;
-  this.score = score;
+// function userCreator(name, score) {
+//   this.name = name;
+//   this.score = score;
+// }
+
+// userCreator.prototype.sayName = function () {
+//   console.log(`I'm ${this.name}`);
+// };
+
+// userCreator.prototype.increment = function () {
+//   this.score++;
+// };
+
+// const user1 = new userCreator('Phil', 5);
+// const user2 = new userCreator('Tim', 4);
+// user1.sayName();
+
+// function paidUserCreator(paidName, paidScore, accountBalance) {
+//   userCreator.call(this, paidName, paidScore);
+//   this.accountBalance = accountBalance;
+// }
+
+// paidUserCreator.prototype = Object.create(userCreator.prototype);
+
+// paidUserCreator.prototype.increaseBalance = function () {
+//   this.accountBalance++;
+// };
+
+// const paidUser1 = new paidUserCreator('Alyysa', 8, 25);
+
+// paidUser1.increaseBalance();
+// console.log(paidUser1.accountBalance);
+// paidUser1.sayName();
+
+// Solution 4
+class userCreator {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  sayName() {
+    console.log(`I'm ${this.name}`);
+  }
+  increment() {
+    this.score++;
+  }
 }
 
-userCreator.prototype.sayName = function () {
-  console.log(`I'm ${this.name}`);
-};
-
-userCreator.prototype.increment = function () {
-  this.score++;
-};
-
-const user1 = new userCreator('Phil', 5);
+const user1 = new userCreator('Phil', 4);
 const user2 = new userCreator('Tim', 4);
 user1.sayName();
 
-function paidUserCreator(paidName, paidScore, accountBalance) {
-  userCreator.call(this, paidName, paidScore);
-  this.accountBalance = accountBalance;
+class paidUserCreatro extends userCreator {
+  constructor(paidName, paidScore, accountBalance) {
+    super(paidName, paidScore);
+    this.accountBalance = accountBalance;
+  }
+
+  increaseBalance() {
+    this.accountBalance++;
+  }
 }
 
-paidUserCreator.prototype = Object.create(userCreator.prototype);
-
-paidUserCreator.prototype.increaseBalance = function () {
-  this.accountBalance++;
-};
-
-const paidUser1 = new paidUserCreator('Alyysa', 8, 25);
+const paidUser1 = new paidUserCreatro('Alyssa', 8, 25);
 
 paidUser1.increaseBalance();
-console.log(paidUser1.accountBalance);
 paidUser1.sayName();
+console.log(paidUser1);
