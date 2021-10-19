5f110d4b97076d6b4ba5fd393f99ded5bc3ce20f
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-19 14:34:29 +0200

    front end master oop course

--- Training/app.js
+++ Training/app.js
@@ -1,8 +1,39 @@
-const obj = {
-  num: 3,
+function userCreator(name, score) {
+  const newUser = Object.create(userFunctions);
+  newUser.name = name;
+  newUser.score = score;
+  return newUser;
+}
+
+userFunctions = {
+  sayName: function () {
+    console.log(`I'm ${this.name}`);
+  },
+  increment: function () {
+    this.score++;
+  },
 };
 
-console.log(obj.num);
-console.log(obj.hasOwnProperty('num'));
+const user1 = userCreator('Phil', 5);
+user1.sayName();
 
-console.log(Object.prototype);
+function paidUserCreator(paidName, paidScore, accountBalance) {
+  const newPaidUser = userCreator(paidName, paidScore);
+  Object.setPrototypeOf(newPaidUser, paidUserFunctions);
+  newPaidUser.accountBalance = accountBalance;
+  return newPaidUser;
+}
+
+const paidUserFunctions = {
+  increaseBalance: function () {
+    this.accountBalance++;
+  },
+};
+
+Object.setPrototypeOf(paidUserFunctions, userFunctions);
+
+const paidUser1 = paidUserCreator('Alyssa', 8, 25);
+
+paidUser1.increaseBalance();
+console.log(paidUser1);
+paidUser1.sayName();
