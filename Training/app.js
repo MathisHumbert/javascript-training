dd60b33f02d812978a0839e4ebcd55e0ed9c2e10
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-19 13:12:36 +0200

    front end master oop course

--- Training/app.js
+++ Training/app.js
@@ -1,24 +1,8 @@
-// My solution
-function mostMoney(students) {
-  let biggest = 0;
+const obj = {
+  num: 3,
+};
 
-  for (let student of students) {
-    let total = student.fives * 5 + student.tens * 10 + student.twenties * 20;
-    if (total > biggest) {
-      biggest = total;
-      person = student.name;
-    } else if (total === biggest) person = 'all';
-  }
+console.log(obj.num);
+console.log(obj.hasOwnProperty('num'));
 
-  return person;
-}
-
-// CW solution
-function mostMoney(s) {
-  s.sort((x, y) => sum(y) - sum(x));
-  if (s.length > 1 && sum(s[0]) == sum(s[1])) return 'all';
-  return s[0].name;
-}
-
-const sum = (s) => s.fives * 5 + s.tens * 10 + s.twenties * 20;
-mostMoney([cam, geoff]);
+console.log(Object.prototype);
