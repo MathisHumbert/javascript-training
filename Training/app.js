22c984975e2c3781c15d8062300ce47cb624f3ff
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-19 15:38:58 +0200

    front end master oop course

--- Training/app.js
+++ Training/app.js
@@ -1,50 +1,85 @@
 // Solution 2
+// function userCreator(name, score) {
+//   const newUser = Object.create(userFunctions);
+//   newUser.name = name;
+//   newUser.score = score;
+//   return newUser;
+// }
+
+// userFunctions = {
+//   sayName: function () {
+//     console.log(`I'm ${this.name}`);
+//   },
+//   increment: function () {
+//     this.score++;
+//   },
+// };
+
+// const user1 = userCreator('Phil', 5);
+// user1.sayName();
+
+// function paidUserCreator(paidName, paidScore, accountBalance) {
+//   const newPaidUser = userCreator(paidName, paidScore);
+//   Object.setPrototypeOf(newPaidUser, paidUserFunctions);
+//   newPaidUser.accountBalance = accountBalance;
+//   return newPaidUser;
+// }
+
+// const paidUserFunctions = {
+//   increaseBalance: function () {
+//     this.accountBalance++;
+//   },
+// };
+
+// Object.setPrototypeOf(paidUserFunctions, userFunctions);
+
+// const paidUser1 = paidUserCreator('Alyssa', 8, 25);
+
+// Call
+// const obj = {
+//   num: 3,
+//   increment: function () {
+//     this.num++;
+//   },
+// };
+
+// const otherObj = { num: 10 };
+// obj.increment();
+// console.log(obj);
+// obj.increment.call(otherObj);
+// console.log(otherObj);
+
+// Solution 3
 function userCreator(name, score) {
-  const newUser = Object.create(userFunctions);
-  newUser.name = name;
-  newUser.score = score;
-  return newUser;
+  this.name = name;
+  this.score = score;
 }
 
-userFunctions = {
-  sayName: function () {
-    console.log(`I'm ${this.name}`);
-  },
-  increment: function () {
-    this.score++;
-  },
+userCreator.prototype.sayName = function () {
+  console.log(`I'm ${this.name}`);
 };
 
-const user1 = userCreator('Phil', 5);
+userCreator.prototype.increment = function () {
+  this.score++;
+};
+
+const user1 = new userCreator('Phil', 5);
+const user2 = new userCreator('Tim', 4);
 user1.sayName();
 
 function paidUserCreator(paidName, paidScore, accountBalance) {
-  const newPaidUser = userCreator(paidName, paidScore);
-  Object.setPrototypeOf(newPaidUser, paidUserFunctions);
-  newPaidUser.accountBalance = accountBalance;
-  return newPaidUser;
+  userCreator.call(this, paidName, paidScore);
+  this.accountBalance = accountBalance;
 }
 
-const paidUserFunctions = {
-  increaseBalance: function () {
-    this.accountBalance++;
-  },
+paidUserCreator.prototype = Object.create(userCreator.prototype);
+
+paidUserCreator.prototype.increaseBalance = function () {
+  this.accountBalance++;
 };
 
-Object.setPrototypeOf(paidUserFunctions, userFunctions);
+const paidUser1 = new paidUserCreator('Alyysa', 8, 25);
 
-const paidUser1 = paidUserCreator('Alyssa', 8, 25);
-
-// Call
-const obj = {
-  num: 3,
-  increment: function () {
-    this.num++;
-  },
-};
-
-const otherObj = { num: 10 };
-obj.increment();
-console.log(obj);
-obj.increment.call(otherObj);
-console.log(otherObj);
+paidUser1.increaseBalance();
+console.log(paidUser1.accountBalance);
+paidUser1.sayName();
