3421983a68dd6fdb93906f50b765bcc7858db1a0
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-21 09:44:28 +0200

    front end master function programing course

--- Training/app.js
+++ Training/app.js
@@ -17,3 +17,19 @@ const copyArrayAndDivideBy2 = (array) => {
 const myArray = [1, 2, 3];
 const result = copyArrayAndMultiplyBy2(myArray);
 const resultBis = copyArrayAndDivideBy2(myArray);
+
+// Solution
+const copyArrayAndManipulate = (array, instructions) => {
+  const output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(instructions(array[i]));
+  }
+  return output;
+};
+
+const multyiplyBy2 = (input) => {
+  return input * 2;
+};
+
+const result3 = copyArrayAndManipulate([1, 2, 3], multyiplyBy2);
+console.log(result3);
