27334f5b06c2511bdb2c7ebbffa8c3ca8e0fd844
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-21 11:19:38 +0200

    front end master function programing course

--- Training/app.js
+++ Training/app.js
@@ -1,5 +1,5 @@
-// Solution
-const copyArrayAndManipulate = (array, instructions) => {
+// map
+const map = (array, instructions) => {
   const output = [];
   for (let i = 0; i < array.length; i++) {
     output.push(instructions(array[i]));
@@ -8,5 +8,18 @@ const copyArrayAndManipulate = (array, instructions) => {
 };
 
 const multyiplyBy2 = (input) => input * 2;
-const result3 = copyArrayAndManipulate([1, 2, 3], multyiplyBy2);
-console.log(result3);
+
+const result = map([1, 2, 3], multyiplyBy2);
+const result2 = map([1, 2, 3], (input) => input * 2);
+
+// reduce
+const reduce = (array, howToCombine, buildingUp) => {
+  for (let i = 0; i < array.length; i++) {
+    buildingUp = howToCombine(buildingUp, array[i]);
+  }
+  return buildingUp;
+};
+
+const add = (a, b) => a + b;
+const summed = reduce([1, 2, 3], add, 0);
+console.log(summed);
