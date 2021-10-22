527e852c6d4f5117563bef052ab8aca4f39157d9
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-22 10:32:27 +0200

    front end master function programing course

--- FEM FP Hard Parts/app.js
+++ FEM FP Hard Parts/app.js
@@ -22,6 +22,26 @@ const capitalize = (str) => str.toUpperCase();
 const addLowerCase = (str) => str + str.toLowerCase();
 const repeat = (str) => str + str;
 
+// highestFun
+const highestFunc = (objOfFuncs, subject) => {
+  let values = {};
+  for (let item in objOfFuncs) {
+    values[item] = objOfFuncs[item](subject);
+  }
+
+  let bigger = Math.max(...Object.values(values));
+  let key = Object.keys(objOfFuncs).find((k) => values[k] === bigger);
+  return key;
+};
+
+// const groupOfFuncs = {};
+// groupOfFuncs.double = (n) => n * 2;
+// groupOfFuncs.addTen = (n) => n + 10;
+// groupOfFuncs.inverse = (n) => n * -1;
+//console.log(highestFunc(groupOfFuncs, 5)); // should log: 'addTen'
+//console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
+//console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
+
 // pipe
 const pipe = (arrOfFuncs, value) => {
   return arrOfFuncs.reduce(runFunctionOnInput, value);
