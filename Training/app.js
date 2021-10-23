8c08571810436545ed67adf2bd2b90435fd93fd8
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-23 11:25:43 +0200

    front end master async course

--- Training/app.js
+++ Training/app.js
@@ -15,3 +15,17 @@ function createNewFunction() {
 const newFunction = createNewFunction();
 const result = newFunction(3);
 console.log(result);
+
+function createFunction(array) {
+  let i = 0;
+  function inner() {
+    const element = array[i];
+    i++;
+    return element;
+  }
+  return inner;
+}
+
+returnNextElement = createFunction([4, 5, 6]);
+const element1 = returnNextElement();
+const element2 = returnNextElement();
