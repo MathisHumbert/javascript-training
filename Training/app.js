61dad7e991105ecc70b4a3c58fe6ffee58381373
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-23 11:01:26 +0200

    front end master async course

--- Training/app.js
+++ Training/app.js
@@ -1,9 +1,17 @@
-const promise1 = new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve('foo');
-  }, 300)
-);
+// iterators
+const numbers = [4, 5, 6];
 
-promise1.then((value) => {
-  console.log(value);
-});
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+function createNewFunction() {
+  function add2(num) {
+    return num + 2;
+  }
+  return add2;
+}
+
+const newFunction = createNewFunction();
+const result = newFunction(3);
+console.log(result);
