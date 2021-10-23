a3ff06363f56fb46750379f2ebd39a240a95f7f0
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-23 14:45:49 +0200

    front end master async course

--- Training/app.js
+++ Training/app.js
@@ -1,16 +1,27 @@
-function createFlow(array) {
-  let i = 0;
-  const inner = {
-    next: function () {
-      const element = array[i];
-      i++;
-      return element;
-    },
-  };
-  return inner;
+// function createFlow(array) {
+//   let i = 0;
+//   const inner = {
+//     next: function () {
+//       const element = array[i];
+//       i++;
+//       return element;
+//     },
+//   };
+//   return inner;
+// }
+
+// const returnNextElement = createFlow([4, 5, 6]);
+// const element1 = returnNextElement.next();
+// const element2 = returnNextElement.next();
+// console.log(element1, element2);
+
+// generator function
+function* createFlow() {
+  yield 4;
+  yield 5;
+  yield 6;
 }
 
 const returnNextElement = createFlow([4, 5, 6]);
 const element1 = returnNextElement.next();
 const element2 = returnNextElement.next();
-console.log(element1, element2);
