08cb8c770480b17f2cae04aa96c64c9fee3dbaad
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-23 14:23:17 +0200

    front end master async course

--- Training/app.js
+++ Training/app.js
@@ -1,31 +1,16 @@
-// iterators
-const numbers = [4, 5, 6];
-
-for (let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
-}
-
-function createNewFunction() {
-  function add2(num) {
-    return num + 2;
-  }
-  return add2;
-}
-
-const newFunction = createNewFunction();
-const result = newFunction(3);
-console.log(result);
-
-function createFunction(array) {
+function createFlow(array) {
   let i = 0;
-  function inner() {
-    const element = array[i];
-    i++;
-    return element;
-  }
+  const inner = {
+    next: function () {
+      const element = array[i];
+      i++;
+      return element;
+    },
+  };
   return inner;
 }
 
-returnNextElement = createFunction([4, 5, 6]);
-const element1 = returnNextElement();
-const element2 = returnNextElement();
+const returnNextElement = createFlow([4, 5, 6]);
+const element1 = returnNextElement.next();
+const element2 = returnNextElement.next();
+console.log(element1, element2);
