ec5b49e4cf19d3c063ddba88541c1b1199dab7f4
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-26 13:21:22 +0200

    front end master functional programing

--- FEM FP Hard Parts/app.js
+++ FEM FP Hard Parts/app.js
@@ -110,6 +110,16 @@ const highestFunc = (objOfFuncs, subject) => {
   return key;
 };
 
+// Challenge 6
+const curriedAddTwoNumsToFive = (num1) => {
+  const starterNum = 5;
+  return (num2) => {
+    return num1 + num2 + starterNum;
+  };
+};
+
+//console.log(curriedAddTwoNumsToFive(6)(7)); // should log 18
+
 // const groupOfFuncs = {};
 // groupOfFuncs.double = (n) => n * 2;
 // groupOfFuncs.addTen = (n) => n + 10;
