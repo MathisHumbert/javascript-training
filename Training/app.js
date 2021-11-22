3db365861c92a5e4f8d2dbe8aebb6eabc8ec2975
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-22 18:16:01 +0100

    optional chaining

--- Training/app.js
+++ Training/app.js
@@ -1,41 +1,15 @@
-// My solution
-function rgb(r, g, b) {
-  let final = [r, g, b];
-  return final
-    .map((item) => {
-      if (item < 0) item = 0;
-      if (item > 255) item = 255;
+const people = [
+  {
+    name: 'bob',
+    location: { street: '123 main street', timezone: { offset: '+7:00' } },
+  },
+  { name: 'peter', location: { street: '123 Pine street' } },
+  {
+    name: 'susan',
+    location: { street: '123 Apple street', timezone: { offset: '+9:00' } },
+  },
+];
 
-      let strItem = '';
-      let first = item / 16;
-      let second = item % 16;
-
-      strItem += returnRGB(first, strItem);
-      strItem += returnRGB(second, strItem);
-
-      return strItem;
-    })
-    .join('');
-}
-
-function returnRGB(num, str) {
-  console.log(num);
-  if (num < 10) {
-    str = Math.floor(num);
-    return str;
-  } else {
-    str = String.fromCharCode(Math.floor(num) + 55);
-    return str;
-  }
-}
-
-// Codewar user solution
-function rgb2(r, g, b) {
-  return toHex(r) + toHex(g) + toHex(b);
-}
-
-function toHex(d) {
-  if (d < 0) return '00';
-  if (d > 255) return 'FF';
-  return (hex = ('0' + Number(d).toString(16)).slice(-2).toUpperCase());
-}
+people.forEach((person) => {
+  console.log(person?.location?.timezone?.offset || 'hellow world');
+});
